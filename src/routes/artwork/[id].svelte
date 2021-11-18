8d32196357cdaf0acabccb995fd93b49efcf39ab
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-11-18 16:41:04 +0200

    Redesigned Artwork Page

--- src/routes/artwork/[id].svelte
+++ src/routes/artwork/[id].svelte
@@ -17,20 +17,19 @@
   import {
     faChevronDown,
     faChevronUp,
-    faTimes,
+    faGift,
+    faShareAlt,
   } from "@fortawesome/free-solid-svg-icons";
   import { getArtwork } from "$queries/artworks";
   import { faHeart, faImage } from "@fortawesome/free-regular-svg-icons";
   import { page } from "$app/stores";
   import { compareAsc, format, parseISO } from "date-fns";
-  import { Activity, Avatar, Card, ProgressLinear, RoyaltyInfo } from "$comp";
+  import { Activity, Avatar, Card, Heart, ProgressLinear, RoyaltyInfo } from "$comp";
   import Sidebar from "./_sidebar.svelte";
   import { tick, onDestroy } from "svelte";
+  import Button from "$styleguide/components/Button.svelte";
   import {
     art,
-    addresses,
-    prompt,
-    password,
     user,
     token,
     psbt,
@@ -113,6 +112,10 @@
 
   $: transaction.amount = sats && sats(amount);
 
+  let stopBidding = async () => {
+    bidding = false;
+  };
+
   let makeOffer = async (e) => {
     try {
       if (e) e.preventDefault();
@@ -154,6 +157,11 @@
 
   let bidding, amountInput, offering;
   let startBidding = async () => {
+    if (!artwork.held)
+      return err(
+          "Can't construct bid transaction, token not currently held in known address for owner"
+      );
+
     bidding = true;
     await tick();
     amountInput.focus();
@@ -161,9 +169,9 @@
 
   let transaction = {
     artwork_id: null,
-    amount: null,
-    type: "bid",
-    hash: "",
+    amount    : null,
+    type      : "bid",
+    hash      : "",
   };
 
   let loading;
@@ -205,16 +213,294 @@
   let showPopup = false;
   let showMore = false;
   let showActivity = false;
+  let actionClassName = null;
 
-</script>
-
-<style>
-  :global(.description a) {
-    color: #3ba5ac;
+  function inDevelopment() {
+    alert('This feature is in development');
   }
 
+  $: actionClassName = artwork && artwork.list_price ? 'buy-now-button-bg' : 'place-bid-button-bg';
+</script>
+
+{#if artwork}
+  <div class="w-full h-96 bg-center bg-cover" style="background-image: url('https://blogs.esa.int/space19plus/files/2019/03/nebula.jpg');">
+
+  </div>
+
+  <div class="nft-container mx-auto pt-24 mb-10">
+    <div class="flex">
+      <div class="w-1/3 mr-16">
+        <div class="overflow-hidden rounded-2xl nft-box-shadow">
+          <ArtworkMedia {artwork} />
+        </div>
+        {#if $user.id === artwork.owner_id}
+          <div class="mt-8">
+            <a href={`/artwork/${id}/edit`}>
+              <Button primary class="w-full">Edit</Button>
+            </a>
+          </div>
+        {/if}
+        <div class="mt-12">
+          <div class="text-2xl font-bold">
+            History
+          </div>
+          <div class="mt-4 text-sm">
+            {#each transactions.slice(0, showActivity ? transactions.length : 3) as transaction}
+              <Activity {transaction} />
+            {/each}
+            {#if transactions.length > 3}
+              <div
+                  class="flex text-xs cursor-pointer"
+                  on:click={() => (showActivity = !showActivity)}
+              >
+                <div>View {showActivity ? "less" : "more"}</div>
+                <div class="my-auto ml-1">
+                  <Fa icon={showActivity ? faChevronUp : faChevronDown} />
+                </div>
+              </div>
+            {/if}
+          </div>
+        </div>
+      </div>
+      <div class="w-3/4 ml-16">
+        <!-- Artwork title -->
+        <div class="font-title">
+          {artwork.title || "Untitled"}
+        </div>
+        <!-- Creator/Owner & controls (like,share,gift)-->
+        <div class="mt-16">
+          <div class="flex">
+            <!-- Avatar -->
+            <div>
+              <a href={`/u/${artwork.artist.username}`} class="text-gray-800 flex">
+                <div>
+                  <Avatar user={artwork.artist} />
+                </div>
+                <div>
+                  <div class="ml-2 h-full flex flex-col justify-center">
+                    <div class="text-sm text-black font-bold">Creator</div>
+                    <div class="text-sm text-gray-800">
+                      @{artwork.artist.username}
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <!-- Avatar -->
+            <div class="ml-24">
+              <a href={`/u/${artwork.owner.username}`} class="text-gray-800 flex">
+                <div>
+                  <Avatar user={artwork.owner} />
+                </div>
+                <div>
+                  <div class="ml-2 h-full flex flex-col justify-center">
+                    <div class="text-sm text-black font-bold">Owner</div>
+                    <div class="text-sm text-gray-800">
+                        @{artwork.owner.username}
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="flex flex-1 justify-end items-center">
+              <div class="mr-1 ml-1 bg-black w-8 h-8 rounded-full flex items-center justify-center text-white cursor-pointer" on:click={inDevelopment}>
+                <Fa icon={faGift}/>
+              </div>
+              <div class="mr-1 ml-1 bg-black w-8 h-8 rounded-full flex items-center justify-center text-white cursor-pointer" on:click={inDevelopment}>
+                <Fa icon={faShareAlt}/>
+              </div>
+              <div class="ml-1 bg-black w-8 h-8 rounded-full flex items-center justify-center text-white cursor-pointer">
+                <Heart {artwork} size="1x"/>
+              </div>
+            </div>
+          </div>
+          <hr class="mt-8 mb-8"/>
+          <!-- price & purchase/make bid -->
+          <div class="flex">
+            <!-- Price block-->
+
+            {#if artwork.list_price || artwork.reserve_price}
+
+              <div class="mr-8">
+                <div class="flex flex-col justify-center align-center bg-black rounded-2xl px-8 h-32">
+
+                  {#if artwork.list_price}
+                    <div class="text-white text-sm mb-4 font-medium">
+                      Sell price
+                    </div>
+                    <div class="text-white text-3xl font-bold flex">
+                      <div>
+                        {list_price}{ticker}
+                      </div>
+                      <div class="text-gray-500 ml-8">
+                        4345.04$
+                      </div>
+                    </div>
+                  {/if}
+
+                  {#if artwork.reserve_price}
+                    <div class="text-white text-sm mb-4 font-medium">
+                      Reserve price
+                    </div>
+                    <div class="text-white text-3xl font-bold flex">
+                      <div>
+                        {val(artwork.reserve_price)}
+                        {ticker}
+                      </div>
+                      <div class="text-gray-500 ml-8">
+                        4345.04$
+                      </div>
+                    </div>
+                  {/if}
+
+                </div>
+              </div>
+
+            {/if}
+
+            <!-- Controls -->
+            <div class="flex flex-1 flex-col">
+
+              <!-- @todo check button's look-->
+              {#if artwork.list_price && (!bidding && !offering)}
+                <input class={`flex justify-center mb-4 items-center inline-block rounded-2xl h-14 text-white font-medium text-center cursor-pointer text-lg ${actionClassName}`}
+                       type="button"
+                       on:click={buyNow}
+                       value="Buy now"
+                       {disabled}
+                       class:disabled/>
+              {/if}
+
+
+              {#if bidding}
+                {#if offering}
+                  <ProgressLinear />
+                {:else}
+                  <form on:submit={makeOffer}>
+                    <div class="flex flex-col mb-4">
+                      <div>
+                        <div class="mt-1 relative rounded-md shadow-sm">
+                          <input
+                              id="price"
+                              class="form-input block w-full pl-7"
+                              placeholder={val(0)}
+                              bind:value={amount}
+                              bind:this={amountInput}
+                          />
+                          <div
+                              class="absolute inset-y-0 right-0 flex items-center mr-2"
+                          >
+                            {ticker}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <input class={`w-full flex justify-center items-center inline-block rounded-2xl h-14 text-white font-medium text-center cursor-pointer text-lg ${actionClassName}`}
+                           type="submit"
+                           value={artwork.list_price ? 'Make an offer' : 'Place bid'}
+                    />
+                    <div class="text-center text-sm cursor-pointer" on:click={stopBidding}>
+                      cancel
+                    </div>
+                  </form>
+                {/if}
+              {:else if !artwork.auction_start || compareAsc(now, parseISO(artwork.auction_start)) === 1}
+                <input class={`w-full flex justify-center items-center inline-block rounded-2xl h-14 text-white font-medium text-center cursor-pointer text-lg ${actionClassName}`}
+                       type="button"
+                       on:click={startBidding}
+                       value={artwork.list_price ? 'Make an offer' : 'Place bid'}
+                       {disabled}
+                       class:disabled
+                />
+              {/if}
+
+              {#if compareAsc(parseISO(artwork.auction_start), now) === 1 && start_counter}
+                <div class="flex flex-1 text-center flex justify-center items-center">
+                  <div class="text-sm">Auction starts in</div>
+                  <div class="ml-2">{start_counter}</div>
+                </div>
+              {:else if compareAsc(parseISO(artwork.auction_end), now) === 1 && end_counter}
+                <div class="flex flex-1 text-center flex justify-center items-center">
+                  <div class="text-sm">Auction closes in</div>
+                  <div class="ml-2">{end_counter}</div>
+                </div>
+              {:else if artwork.auction_end}
+                <div class="flex flex-1 text-center flex justify-center items-center">
+                  <div class="text-sm">Auction ended at</div>
+                  <div class="ml-2">
+                    {format(parseISO(artwork.auction_end), "yyyy-MM-dd HH:mm")}
+                  </div>
+                </div>
+              {/if}
+
+            </div>
+          </div>
+          <!-- Description -->
+          <div class="mt-12">
+            <div class="text-2xl font-bold">Description</div>
+            <div class="mt-4">
+              {@html linkify(artwork.description) || 'Description of this product is empty'}
+            </div>
+          </div>
+
+          <!-- Package content -->
+          <div class="mt-12">
+            <div class="text-2xl font-bold">Package content</div>
+            <div class="mt-4">
+              {artwork.package_content || 'Package content of this product is empty'}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-72"></div>
+  </div>
+
+{:else}
+  <ProgressLinear />
+{/if}
+
+<style lang="scss">
+
   .disabled {
-    @apply text-gray-400 border-gray-400;
+    cursor: not-allowed;
+    opacity: 0.8;
+  }
+
+  .nft-container {
+    max-width: 1120px;
+  }
+
+  .place-bid-button-bg {
+    background: rgb(254,96,0);
+    background: linear-gradient(225deg, rgba(254,96,0,1) 0%, rgba(254,1,42,1) 100%);
+  }
+
+  .buy-now-button-bg {
+    background: rgb(191,1,191);
+    background: linear-gradient(225deg, rgba(191,1,191,1) 0%, rgba(13,101,191,1) 100%);
+
+  }
+
+  .font-title {
+    font-family: "Zen Dots", cursive;
+    font-size: 2em;
+    line-height: 1.25em;
+  }
+
+  .nft-box-shadow {
+    box-shadow: 6px 6px 6px 0 rgba(0, 0, 0, .3);
+  }
+  .listContainer {
+    overflow: hidden;
+  }
+
+  svelte-virtual-list-viewport {
+    overflow: hidden;
+  }
+
+  :global(.description a) {
+    color: #3ba5ac;
   }
 
   button {
