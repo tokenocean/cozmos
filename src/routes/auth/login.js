3725fcee0a576f731a100bc1a38fc3abd27b455c
Adam Soltys <asoltys@gmail.com>
2021-12-01 00:33:43 -0800

    set jwt cookie on login and refresh

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -9,13 +9,25 @@ export async function post(request) {
     let cookies = [res.headers.get('set-cookie').split(',').slice(0, 2).join("")];
     headers['set-cookie'] = cookies;
 
-    console.log("LOGIN", headers);
+    let tokenExpiry = parseInt(jwt_expires_in / 1000);
 
     return {
-      body: await res.json(),
-      headers,
+      body,
+      headers: {
+        "set-cookie": [
+          res.headers.get("set-cookie").split(",").slice(0, 2).join(""),
+          cookie.serialize("token", jwt_token, {
+            httpOnly: true,
+            maxAge: tokenExpiry,
+            sameSite: "lax",
+            path: "/",
+            expires: addSeconds(new Date(), tokenExpiry),
+          }),
+        ],
+      },
     };
   } catch (e) {
+    console.log(e);
     return {
       body: { message: "Login failed" },
       status: 500,
