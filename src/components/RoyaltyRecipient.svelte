2f4443bb7582dc7961368359d7a926e871b86094
Adam Soltys <asoltys@gmail.com>
2021-12-04 01:10:47 -0800

    latest royalty components

--- src/components/RoyaltyRecipient.svelte
+++ src/components/RoyaltyRecipient.svelte
@@ -1,9 +1,10 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Fa from "svelte-fa";
   import { faTimes } from "@fortawesome/free-solid-svg-icons";
-  import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let recipient;
+  export let editable = true;
   import { tickers, royaltyRecipientIndividualType } from "$lib/utils";
   const onDelete = () => dispatch("removerecipient", recipient.name);
 
@@ -23,15 +24,18 @@
     class="flex flex-wrap w-full bg-gray-600 text-gray-100 rounded-r-lg p-4 border-white">
     <div class="flex flex-wrap flex-grow">
       <div>{recipient.name}</div>
-      <div class="ml-auto my-auto">{recipient.amount}%</div>
+      <div class="ml-auto my-auto">
+        {recipient.amount}%
+      </div>
       <div class="w-full break-all text-wrap text-xs text-secondary">
         {recipient.address}
       </div>
     </div>
-    {#if recipient.type === royaltyRecipientIndividualType}
+    {#if editable && recipient.type === royaltyRecipientIndividualType}
       <div
         class="btn cursor-pointer text-center rounded-sm text-sm p-2 my-auto ml-2"
-        on:click|stopPropagation={onDelete}>
+        on:click|stopPropagation={onDelete}
+      >
         <Fa icon={faTimes} />
       </div>
     {/if}
