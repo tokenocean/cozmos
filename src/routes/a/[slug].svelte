6eab50958903bd1c69dc27cd06d34284331677d9
Adam Soltys <asoltys@gmail.com>
2021-12-13 18:44:21 -0800

    misc fixes

--- src/routes/a/[slug].svelte
+++ src/routes/a/[slug].svelte
@@ -14,7 +14,13 @@
         status: 404,
       };
 
-    serverApi.url("/viewed").post({ id: artwork.id }).json().catch(console.log);
+    if (!browser)
+      post("/artworks/viewed", { id: artwork.id }, fetch)
+        .res()
+        .catch(console.log);
+
+    artwork.views++;
+    props.views = artwork.views;
 
     let metadata = { ...branding.meta };
     metadata.title = metadata.title + " - " + artwork.title;
@@ -79,7 +85,7 @@
 
   export let artwork, metadata;
 
-  $: disabled =
+  $: disabled = loading ||
     !artwork ||
     artwork.transactions.some(
       (t) => ["purchase", "creation", "cancel"].includes(t.type) && !t.confirmed
@@ -137,22 +143,27 @@
   let val, sats, ticker;
   let amount;
 
-  $: transaction.amount = sats && sats(amount);
-
+  let transaction = {};
   let makeOffer = async (e) => {
     try {
       if (e) e.preventDefault();
       offering = true;
+
+      transaction.amount = sats(amount);
+      transaction.asset = artwork.asset;
       transaction.type = "bid";
 
       await requirePassword();
 
       $psbt = await createOffer(artwork, transaction.amount);
       $psbt = await sign();
+
       transaction.psbt = $psbt.toBase64();
-      transaction.hash = $psbt.__CACHE.__TX.getId();
+      transaction.hash = $psbt.data.globalMap.unsignedTx.tx.getId();
+
       await save();
       await fetch();
+
       offering = false;
     } catch (e) {
       console.log(e);
@@ -185,13 +196,6 @@
     amountInput.focus();
   };
 
-  let transaction = {
-    artwork_id: null,
-    amount: null,
-    type: "bid",
-    hash: "",
-  };
-
   let loading;
   let buyNow = async () => {
     try {
@@ -199,6 +203,7 @@
       loading = true;
 
       transaction.amount = -artwork.list_price;
+      transaction.asset = artwork.asset;
       transaction.type = "purchase";
 
       $psbt = await executeSwap(artwork);
@@ -215,11 +220,6 @@
       transaction.psbt = $psbt.toBase64();
 
       await save();
-
-      transaction.amount = 1;
-      transaction.asset = artwork.asset;
-      transaction.user;
-
       await fetch();
     } catch (e) {
       err(e);
@@ -234,7 +234,6 @@
 
 </script>
 
-<!--<script ✂prettier:content✂="CiAgaW1wb3J0IEZhIGZyb20gInN2ZWx0ZS1mYSI7CiAgaW1wb3J0IHsKICAgIGZhQ2hldnJvbkRvd24sCiAgICBmYUNoZXZyb25VcCwKICAgIGZhR2lmdCwKICAgIGZhU2hhcmVBbHQsCiAgfSBmcm9tICJAZm9ydGF3ZXNvbWUvZnJlZS1zb2xpZC1zdmctaWNvbnMiOwogIGltcG9ydCB7IGZhVGltZXMgfSBmcm9tICJAZm9ydGF3ZXNvbWUvZnJlZS1zb2xpZC1zdmctaWNvbnMiOwogIGltcG9ydCB7IGdldEFydHdvcmsgfSBmcm9tICIkcXVlcmllcy9hcnR3b3JrcyI7CiAgaW1wb3J0IHsgZmFIZWFydCwgZmFJbWFnZSB9IGZyb20gIkBmb3J0YXdlc29tZS9mcmVlLXJlZ3VsYXItc3ZnLWljb25zIjsKICBpbXBvcnQgeyBwYWdlIH0gZnJvbSAiJGFwcC9zdG9yZXMiOwogIGltcG9ydCB7IGNvbXBhcmVBc2MsIGZvcm1hdCwgcGFyc2VJU08gfSBmcm9tICJkYXRlLWZucyI7CiAgaW1wb3J0IHsKICAgIEFjdGl2aXR5LAogICAgQXJ0d29ya01lZGlhLAogICAgQXZhdGFyLAogICAgSGVhZCwKICAgIEhlYXJ0LAogICAgUHJvZ3Jlc3NMaW5lYXIsCiAgICBSb3lhbHR5SW5mbywKICB9IGZyb20gIiRjb21wIjsKICBpbXBvcnQgU2lkZWJhciBmcm9tICIuL19zaWRlYmFyLnN2ZWx0ZSI7CiAgaW1wb3J0IHsgdGljaywgb25EZXN0cm95IH0gZnJvbSAic3ZlbHRlIjsKICBpbXBvcnQgQnV0dG9uIGZyb20gIiRzdHlsZWd1aWRlL2NvbXBvbmVudHMvQnV0dG9uLnN2ZWx0ZSI7CiAgaW1wb3J0IENhcmQgZnJvbSAiJHN0eWxlZ3VpZGUvY29tcG9uZW50cy9DYXJkLnN2ZWx0ZSI7CiAgaW1wb3J0IHsgYXJ0LCB1c2VyLCB0b2tlbiwgcHNidCB9IGZyb20gIiRsaWIvc3RvcmUiOwogIGltcG9ydCBjb3VudGRvd24gZnJvbSAiJGxpYi9jb3VudGRvd24iOwogIGltcG9ydCB7IGdvdG8sIGVyciwgZXhwbG9yZXIsIGluZm8sIGxpbmtpZnksIHVuaXRzIH0gZnJvbSAiJGxpYi91dGlscyI7CiAgaW1wb3J0IHsgcmVxdWlyZVBhc3N3b3JkIH0gZnJvbSAiJGxpYi9hdXRoIjsKICBpbXBvcnQgewogICAgY3JlYXRlT2ZmZXIsCiAgICBleGVjdXRlU3dhcCwKICAgIHJlcXVlc3RTaWduYXR1cmUsCiAgICBzaWduLAogICAgYnJvYWRjYXN0LAogIH0gZnJvbSAiJGxpYi93YWxsZXQiOwogIGltcG9ydCB7IFBzYnQgfSBmcm9tICJsaXF1aWRqcy1saWIiOwogIGltcG9ydCB7IGFwaSwgcXVlcnkgfSBmcm9tICIkbGliL2FwaSI7CiAgaW1wb3J0IHsgU29jaWFsU2hhcmUgfSBmcm9tICIkY29tcCI7CgogIGV4cG9ydCBsZXQgYXJ0d29yaywgbWV0YWRhdGE7CgogICQ6IGRpc2FibGVkID0KICAgICFhcnR3b3JrIHx8CiAgICBhcnR3b3JrLnRyYW5zYWN0aW9ucy5zb21lKAogICAgICAodCkgPT4gWyJwdXJjaGFzZSIsICJjcmVhdGlvbiIsICJjYW5jZWwiXS5pbmNsdWRlcyh0LnR5cGUpICYmICF0LmNvbmZpcm1lZAogICAgKTsKCiAgbGV0IHN0YXJ0X2NvdW50ZXIsIGVuZF9jb3VudGVyLCBub3csIHRpbWVvdXQsIGxvYWRlZDsKCiAgbGV0IGlkID0gYXJ0d29yayA/IGFydHdvcmsuaWQgOiAkcGFnZS5wYXJhbXMuaWQ7CgogIG9uRGVzdHJveSgoKSA9PiB7CiAgICAkYXJ0ID0gdW5kZWZpbmVkOwogICAgY2xlYXJJbnRlcnZhbChwb2xsKTsKICB9KTsKCiAgLyoKICAkOiB1cGRhdGUoYXJ0d29yayk7CiAgbGV0IHVwZGF0ZSA9ICgpID0+IHsKICAgIGlmICghYXJ0d29yaykgcmV0dXJuOwogICAgJGFydCA9IGFydHdvcms7CgogICAgbGV0IGNvdW50ID0gKCkgPT4gewogICAgICBjbGVhclRpbWVvdXQodGltZW91dCk7CiAgICAgIG5vdyA9IG5ldyBEYXRlKCk7CiAgICAgIGlmICghYXJ0d29yaykgcmV0dXJuOwogICAgICBzdGFydF9jb3VudGVyID0gY291bnRkb3duKHBhcnNlSVNPKGFydHdvcmsuYXVjdGlvbl9zdGFydCkpIHx8ICIiOwogICAgICBlbmRfY291bnRlciA9IGNvdW50ZG93bihwYXJzZUlTTyhhcnR3b3JrLmF1Y3Rpb25fZW5kKSkgfHwgIiI7CiAgICAgIHRpbWVvdXQgPSBzZXRUaW1lb3V0KGNvdW50LCAxMDAwKTsKICAgIH07CiAgICBjb3VudCgpOwoKICAgIFtzYXRzLCB2YWwsIHRpY2tlcl0gPSB1bml0cyhhcnR3b3JrICYmIGFydHdvcmsuYXNraW5nX2Fzc2V0KTsKICAgIGxpc3RfcHJpY2UgPSBhcnR3b3JrLmxpc3RfcHJpY2U7CiAgICBsaXN0X3ByaWNlID0gdmFsKGFydHdvcmsubGlzdF9wcmljZSk7CiAgfTsKCiAgbGV0IGxpc3RfcHJpY2U7CiAgbGV0IHZhbCwgc2F0cywgdGlja2VyOwogIGxldCBhbW91bnQ7CgogICQ6IHRyYW5zYWN0aW9uLmFtb3VudCA9IHNhdHMgJiYgc2F0cyhhbW91bnQpOwoKICBsZXQgc3RvcEJpZGRpbmcgPSBhc3luYyAoKSA9PiB7CiAgICBiaWRkaW5nID0gZmFsc2U7CiAgfTsKCiAgbGV0IG1ha2VPZmZlciA9IGFzeW5jIChlKSA9PiB7CiAgICB0cnkgewogICAgICBpZiAoZSkgZS5wcmV2ZW50RGVmYXVsdCgpOwogICAgICBvZmZlcmluZyA9IHRydWU7CiAgICAgIHRyYW5zYWN0aW9uLnR5cGUgPSAiYmlkIjsKCiAgICAgIGF3YWl0IHJlcXVpcmVQYXNzd29yZCgpOwoKICAgICAgJHBzYnQgPSBhd2FpdCBjcmVhdGVPZmZlcihhcnR3b3JrLCB0cmFuc2FjdGlvbi5hbW91bnQpOwogICAgICAkcHNidCA9IGF3YWl0IHNpZ24oKTsKICAgICAgdHJhbnNhY3Rpb24ucHNidCA9ICRwc2J0LnRvQmFzZTY0KCk7CiAgICAgIHRyYW5zYWN0aW9uLmhhc2ggPSAkcHNidC5fX0NBQ0hFLl9fVFguZ2V0SWQoKTsKICAgICAgYXdhaXQgc2F2ZSgpOwogICAgICBhd2FpdCBmZXRjaCgpOwogICAgICBvZmZlcmluZyA9IGZhbHNlOwogICAgfSBjYXRjaCAoZSkgewogICAgICBjb25zb2xlLmxvZyhlKTsKICAgICAgZXJyKGUpOwogICAgICBvZmZlcmluZyA9IGZhbHNlOwogICAgfQogIH07CgogIGxldCBzYXZlID0gYXN5bmMgKGUpID0+IHsKICAgIHRyYW5zYWN0aW9uLmFydHdvcmtfaWQgPSBhcnR3b3JrLmlkOwogICAgdHJhbnNhY3Rpb24uYXNzZXQgPSBhcnR3b3JrLmFza2luZ19hc3NldDsKCiAgICBsZXQgeyBkYXRhLCBlcnJvcnMgfSA9IGF3YWl0IGFwaQogICAgICAuYXV0aChgQmVhcmVyICR7JHRva2VufWApCiAgICAgIC51cmwoIi90cmFuc2FjdGlvbiIpCiAgICAgIC5wb3N0KHsgdHJhbnNhY3Rpb24gfSkKICAgICAgLmpzb24oKTsKCiAgICBpZiAoZXJyb3JzKSB0aHJvdyBuZXcgRXJyb3IoZXJyb3JzWzBdLm1lc3NhZ2UpOwoKICAgIGlmICh0cmFuc2FjdGlvbi50eXBlID09PSAicHVyY2hhc2UiKSBpbmZvKCJTb2xkISBDb25ncmF0dWxhdGlvbnMhIik7CiAgICBpZiAodHJhbnNhY3Rpb24udHlwZSA9PT0gImJpZCIpIGluZm8oIkJpZCBwbGFjZWQhIik7CiAgICBiaWRkaW5nID0gZmFsc2U7CiAgfTsKCiAgbGV0IGJpZGRpbmcsIGFtb3VudElucHV0LCBvZmZlcmluZzsKICBsZXQgc3RhcnRCaWRkaW5nID0gYXN5bmMgKCkgPT4gewogICAgaWYgKCFhcnR3b3JrLmhlbGQpCiAgICAgIHJldHVybiBlcnIoCiAgICAgICAgIkNhbid0IGNvbnN0cnVjdCBiaWQgdHJhbnNhY3Rpb24sIHRva2VuIG5vdCBjdXJyZW50bHkgaGVsZCBpbiBrbm93biBhZGRyZXNzIGZvciBvd25lciIKICAgICAgKTsKCiAgICBiaWRkaW5nID0gdHJ1ZTsKICAgIGF3YWl0IHRpY2soKTsKICAgIGFtb3VudElucHV0LmZvY3VzKCk7CiAgfTsKCiAgbGV0IHRyYW5zYWN0aW9uID0gewogICAgYXJ0d29ya19pZDogbnVsbCwKICAgIGFtb3VudDogbnVsbCwKICAgIHR5cGU6ICJiaWQiLAogICAgaGFzaDogIiIsCiAgfTsKCiAgbGV0IGxvYWRpbmc7CiAgbGV0IGJ1eU5vdyA9IGFzeW5jICgpID0+IHsKICAgIHRyeSB7CiAgICAgIGF3YWl0IHJlcXVpcmVQYXNzd29yZCgpOwogICAgICBsb2FkaW5nID0gdHJ1ZTsKCiAgICAgIHRyYW5zYWN0aW9uLmFtb3VudCA9IC1hcnR3b3JrLmxpc3RfcHJpY2U7CiAgICAgIHRyYW5zYWN0aW9uLnR5cGUgPSAicHVyY2hhc2UiOwoKICAgICAgJHBzYnQgPSBhd2FpdCBleGVjdXRlU3dhcChhcnR3b3JrKTsKICAgICAgJHBzYnQgPSBhd2FpdCBzaWduKCk7CgogICAgICBpZiAoYXJ0d29yay5oYXNfcm95YWx0eSB8fCBhcnR3b3JrLmF1Y3Rpb25fZW5kKSB7CiAgICAgICAgJHBzYnQgPSBhd2FpdCByZXF1ZXN0U2lnbmF0dXJlKCRwc2J0KTsKICAgICAgfQoKICAgICAgYXdhaXQgYnJvYWRjYXN0KCRwc2J0KTsKCiAgICAgIGxldCB0eCA9ICRwc2J0LmV4dHJhY3RUcmFuc2FjdGlvbigpOwogICAgICB0cmFuc2FjdGlvbi5oYXNoID0gdHguZ2V0SWQoKTsKICAgICAgdHJhbnNhY3Rpb24ucHNidCA9ICRwc2J0LnRvQmFzZTY0KCk7CgogICAgICBhd2FpdCBzYXZlKCk7CgogICAgICB0cmFuc2FjdGlvbi5hbW91bnQgPSAxOwogICAgICB0cmFuc2FjdGlvbi5hc3NldCA9IGFydHdvcmsuYXNzZXQ7CiAgICAgIHRyYW5zYWN0aW9uLnVzZXI7CgogICAgICBhd2FpdCBmZXRjaCgpOwogICAgfSBjYXRjaCAoZSkgewogICAgICBlcnIoZSk7CiAgICB9CgogICAgbG9hZGluZyA9IGZhbHNlOwogIH07CgogIGxldCBzaG93UG9wdXAgPSBmYWxzZTsKICBsZXQgc2hvd01vcmUgPSBmYWxzZTsKICBsZXQgc2hvd0FjdGl2aXR5ID0gZmFsc2U7CiAgbGV0IGFjdGlvbkNsYXNzTmFtZSA9IG51bGw7CgogIGZ1bmN0aW9uIGluRGV2ZWxvcG1lbnQoKSB7CiAgICBhbGVydCgiVGhpcyBmZWF0dXJlIGlzIGluIGRldmVsb3BtZW50Iik7CiAgfQoKICAkOiBhY3Rpb25DbGFzc05hbWUgPQogICAgYXJ0d29yayAmJiBhcnR3b3JrLmxpc3RfcHJpY2UgPyAiYnV5LW5vdy1idXR0b24tYmciIDogInBsYWNlLWJpZC1idXR0b24tYmciOwoK">{}</script>-->
 <style lang="scss">
   .disabled {
     cursor: not-allowed;
