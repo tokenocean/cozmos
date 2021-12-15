33d5f0c427fe63c2e2e61764e3495a84b22998be
Adam Soltys <asoltys@gmail.com>
2021-12-15 10:42:47 -0800

    new royalties icon

--- src/components/RoyaltyInfo.svelte
+++ src/components/RoyaltyInfo.svelte
@@ -1,7 +1,6 @@
 <script>
   import RoyaltyRecipient from "$components/RoyaltyRecipient.svelte";
   import Fa from "svelte-fa";
-  import { faGem } from "@fortawesome/free-regular-svg-icons";
   import {
     faChevronUp,
     faChevronDown,
@@ -11,15 +10,15 @@
 
 </script>
 
-<div class="mb-6">
-  <div class="text-xs cursor-pointer mb-2" on:click={() => (show = !show)}>
-    <div class="flex">
-      <i class="text-midblue text-xs mr-1 my-auto">
-        <Fa icon={faGem} />
-      </i>
-      <div class="my-auto">Royalties</div>
-      <div class="my-auto ml-1">
-        <Fa icon={show ? faChevronUp : faChevronDown} />
+{#if artwork.royalty_recipients.length}
+  <div class="mb-6">
+    <div class="text-xs cursor-pointer mb-2" on:click={() => (show = !show)}>
+      <div class="flex">
+        <img src="/royalties.svg" class="w-8" />
+        <div class="my-auto">Royalties</div>
+        <div class="my-auto ml-1">
+          <Fa icon={show ? faChevronUp : faChevronDown} />
+        </div>
       </div>
     </div>
   </div>
@@ -30,10 +29,9 @@
           <RoyaltyRecipient
             editable={false}
             askingAsset={artwork.asking_asset}
-            {recipient}
-          />
+            {recipient} />
         {/each}
       {/if}
     </ul>
   {/if}
-</div>
+{/if}
