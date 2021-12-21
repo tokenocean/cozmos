945533a20cade46ff53b928741a53c5bca72d1e6
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-12-21 14:30:42 -0700

    Create thank you component for after a user submits an experience

---
+++ src/components/ThankYou.svelte
@@ -0,0 +1,35 @@
+<div class="container text-center bg-gray-100 p-20">
+  <h2 class='mb-6'>Great!!</h2>
+  <h4 class="font-bold text-2xl mb-6">
+    Thanks for submitting your experience.<br />
+    We appreciate it.
+  </h4>
+  <p class='text-xl'>It might take a few minutes while we review it.<br /> Good luck!</p>
+  <img
+    src="/static/svg_icons/Logo.svg"
+    alt="Cozmos logo"
+    class="w-40 h-40 mx-auto" />
+  <div class="border-t-2 border-gray-400 flex justify-center">
+    <div class="flex mt-10">
+      <a href="#">
+        <img
+          src="/static/svg_icons/Twitter-02.svg"
+          alt="Twitter icon"
+          class="w-16 h-16" />
+      </a>
+      <a href="#">
+        <img
+          src="/static/svg_icons/Instagram-02.svg"
+          alt="Instagram icon"
+          class="w-16 h-16" />
+      </a>
+      <a href="#">
+        <img
+          src="/static/svg_icons/Facebook-02.svg"
+          alt="Facebook icon"
+          class="w-16 h-16" />
+      </a>
+    </div>
+    <p class='self-center ml-24 text-xl mt-10'>Follow us for updates</p>
+  </div>
+</div>
