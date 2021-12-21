9076b438a594c99affc1555524a8ddf7ddd73d43
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-12-21 15:45:06 -0700

    Create connect wallet popup component

---
+++ src/components/ConnectWallet.svelte
@@ -0,0 +1,51 @@
+<style media="screen">
+  .marina {
+    background-color: #24969c;
+  }
+
+</style>
+
+<div class="container text-center bg-gray-100 p-20">
+  <h2 class="mb-6">Connect your wallet</h2>
+  <p class="text-xl">
+    By connecting your wallet, you agree to our Terms of Service and our Privacy
+    Policy.
+  </p>
+  <button
+    type="button"
+    name="button"
+    class="bg-black block mx-auto sm:w-full md:w-5/6 md:h-16 mt-16 mb-4 rounded text-white font-bold marina flex items-center md:text-xl"><img
+      src="/static/svg_icons/Marina.svg"
+      alt="Marina icon"
+      class="w-20 h-20 md:mr-20 lg:mr-32" />Marina - Liquid Wallet</button>
+  <button
+    type="button"
+    name="button"
+    class="bg-black block mx-auto sm:w-full md:w-5/6 md:h-16 rounded text-white font-bold flex items-center md:text-xl"><img
+      src="/static/svg_icons/Coinos.svg"
+      alt="coinos icon"
+      class="w-20 h-20 md:mr-20 lg:mr-32" />Create one with us</button>
+  <div class="block md:flex justify-center">
+    <div class="flex mt-10">
+      <a href="#">
+        <img
+          src="/static/svg_icons/Twitter-02.svg"
+          alt="Twitter icon"
+          class="w-52 h-52 md:w-16 md:h-16" />
+      </a>
+      <a href="#">
+        <img
+          src="/static/svg_icons/Instagram-02.svg"
+          alt="Instagram icon"
+          class="w-52 h-52 md:w-16 md:h-16" />
+      </a>
+      <a href="#">
+        <img
+          src="/static/svg_icons/Facebook-02.svg"
+          alt="Facebook icon"
+          class="w-52 h-52 md:w-16 md:h-16" />
+      </a>
+    </div>
+    <p class="self-center md:ml-24 text-xl md:mt-10">Follow us for updates</p>
+  </div>
+</div>
