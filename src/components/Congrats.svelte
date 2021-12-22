4d53f609bae92374bb6860ff1595bc57d0426162
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-12-21 22:33:47 -0700

    Create congrats component for when you buy an experience

---
+++ src/components/Congrats.svelte
@@ -0,0 +1,37 @@
+<div class="container text-center bg-gray-100 p-20">
+  <div class="flex justify-center">
+    <h2 class="mb-6">Congratulations!!</h2>
+  </div>
+  <p class="text-xl">You just purchased<br /> VIP Backstage at Tomorrowland.</p>
+  <img
+    src="/static/tomorrowland.jpg"
+    alt="Tomorrowland picture"
+    class="w-120 md:w-90 md:h-60 mx-auto rounded-t-xl my-10" />
+  <p class="text-xl">Status: complete</p>
+  <div class="border-t-2 border-gray-400 block md:flex justify-center mt-10">
+    <div class="flex mt-5 md:mt-10">
+      <a href="https://twitter.com/cozmosnft" target="_blank" rel="noreferrer">
+        <img
+          src="/static/svg_icons/Twitter-02.svg"
+          alt="Twitter icon"
+          class="w-52 md:w-16" />
+      </a>
+      <a
+        href="https://instagram.com/cozmosnft"
+        target="_blank"
+        rel="noreferrer">
+        <img
+          src="/static/svg_icons/Instagram-02.svg"
+          alt="Instagram icon"
+          class="w-52 md:w-16" />
+      </a>
+      <a href="https://facebook.com/cozmosnft" target="_blank" rel="noreferrer">
+        <img
+          src="/static/svg_icons/Facebook-02.svg"
+          alt="Facebook icon"
+          class="w-52 md:w-16" />
+      </a>
+    </div>
+    <p class="self-center md:ml-24 text-xl md:mt-10">Follow us for updates</p>
+  </div>
+</div>
