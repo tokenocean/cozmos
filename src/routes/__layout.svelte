3e71b903ada7d2d286df7f4e3aec0544d9656459
Adam Soltys <asoltys@gmail.com>
2022-01-04 05:44:53 -0800

    get always returns json

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,17 @@
 <script context="module">
-  import { prerendering } from '$app/env';
+  import { prerendering } from "$app/env";
+  import { get } from "$lib/api";
+
   export async function load({ fetch, page, session }) {
-    if (prerendering) return {
-      props: {
-        addresses: [],
-        titles: []
-      } 
-    } 
-    
-    const props = await fetch(`/addresses.json`).then((r) => r.json());
+    if (prerendering)
+      return {
+        props: {
+          addresses: [],
+          titles: [],
+        },
+      };
+
+    const props = await get(`/addresses.json`, fetch);
 
     if (
       session &&
@@ -53,7 +56,7 @@
 
   let refresh = async () => {
     try {
-      let { jwt_token } = await get("/auth/refresh.json", fetch).json();
+      let { jwt_token } = await get("/auth/refresh.json", fetch);
       $token = jwt_token;
       if (!$token && $session) delete $session.user;
     } catch (e) {
