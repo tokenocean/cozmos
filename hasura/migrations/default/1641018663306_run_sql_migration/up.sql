833553ec7bb393090af368a756ed9a396e790021
Adam Soltys <asoltys@gmail.com>
2022-01-11 11:31:34 -0800

    fix broken migrations

--- hasura/migrations/default/1641018663306_run_sql_migration/up.sql
+++ hasura/migrations/default/1641018663306_run_sql_migration/up.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE
 OR REPLACE VIEW "public"."currentuser" AS
 SELECT
@@ -33,31 +32,3 @@ SELECT
   users.last_seen_tx
 FROM
   users;
-=======
-CREATE OR REPLACE VIEW "public"."currentuser" AS 
- SELECT users.id,
-    users.created_at,
-    users.updated_at,
-    users.display_name,
-    users.avatar_url,
-    users.username,
-    users.location,
-    users.bio,
-    users.website,
-    users.email,
-    users.full_name,
-    users.mnemonic,
-    users.address,
-    users.multisig,
-    users.is_artist,
-    users.is_admin,
-    users.wallet_initialized,
-    users.twitter,
-    users.instagram,
-    users.pubkey,
-    users.info,
-    users.confidential,
-    users.blindkey,
-    users.last_seen_tx
-   FROM users;
->>>>>>> 36b0a351... utxos table and last seen tx column
