1192f2da4bb49dd1ae915dbbc73a270ff14b353d
Adam Soltys <asoltys@gmail.com>
2022-01-27 13:56:05 -0800

    move market to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,106 @@
-<script>
-  import Button from "$styleguide/components/Button.svelte";
+<script context="module">
+  import { post } from "$lib/api";
+  export async function load({ fetch }) {
+    const r = await post("/artworks.json", {}, fetch).json();
+
+    return {
+      props: {
+        total: r.total,
+        initialArtworks: r.artworks,
+      },
+    };
+  }
 </script>
 
-<div
-  class="overflow-hidden md:overflow-visible min-h-screen flex header-container mx-auto justify-center items-center bg-black"
->
-  <div class="flex justify-center mx-24 -mt-32">
-    <div class="leading-normal">
-      <div class="text-gray-500 text-xl font-semibold ml-20 md:ml-0">
-        WE ARE DISRUPTING THE NFT INDUSTRY.
-      </div>
-      <div
-        class="text-white text-5xl secondary-font leading-normal mt-4 ml-20 md:ml-0"
-      >
-        The First marketplace<br />
-        for NFT-Experiences
-      </div>
-      <div class="text-white text-xl secondary-font mt-4 ml-20 md:ml-0">
-        From creators for everyone
-      </div>
-      <div class="text-gray-500 text-base font-semibold mt-6 ml-20 md:ml-0">
-        An NFT marketplace with real-life experiences
-        <br />based on the L-BTC blockchain.
-      </div>
-      <div class="mt-8 ml-20 md:ml-0">
-        <a href="/market">
-          <Button primary>Start Exploring</Button>
-        </a>
-      </div>
-    </div>
-    <div class="flex flex-1 ml-20 sm:hidden xl:flex items-center">
-      <div>
-        <a href="/market">
-          <img src="/cards.png" class="w-full max-h-96" alt="Cards" />
-        </a>
-      </div>
-    </div>
-  </div>
+<script>
+  import { ProgressLinear } from "$comp";
+  import Fa from "svelte-fa";
+  import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
+  import {
+    artworks,
+    filterCriteria as fc,
+    offset,
+    results,
+    show,
+    sortCriteria as sc,
+    token,
+    user,
+  } from "$lib/store";
+  import { info, err, goto } from "$lib/utils";
+  import { Gallery, Results, Search } from "$comp";
+  import Filter from "./_filter.svelte";
+  import Sort from "./_sort.svelte";
+  import { requirePassword } from "$lib/auth";
+  import { compareAsc, differenceInMilliseconds, parseISO } from "date-fns";
+  import { browser } from "$app/env";
+
+  export let total;
+  export let initialArtworks = [];
+
+  let showFilters;
+  let filtered = [...initialArtworks];
+
+  $: filtersUpdated($fc, $sc);
+  let filtersUpdated = () => {
+    $offset = 0;
+    loadMore();
+  };
+
+  let loadMore = async () => {
+    if (!browser) return;
+    try {
+      let where = {};
+      if ($sc === "ending_soon")
+        where.auction_end = { _is_null: false, _gte: new Date() };
+      if ($fc.listPrice || ["lowest", "highest"].includes($sc)) {
+        $fc.listPrice = true;
+        where.list_price = { _is_null: false, _gt: 0 };
+      }
+      if ($fc.openBid) where.bid_id = { _is_null: false };
+      if ($fc.ownedByCreator) where.artist_owned = { _eq: true };
+      if ($fc.hasSold) where.transferred_at = { _is_null: false };
+
+      let order_by = {
+        newest: { created_at: "desc" },
+        oldest: { created_at: "asc" },
+        highest: { list_price: "desc" },
+        lowest: { list_price: "asc" },
+        ending_soon: { auction_end: "asc" },
+        most_viewed: { views: "desc" },
+      }[$sc];
+
+      const r = await post(
+        "/artworks.json",
+        { offset: $offset, order_by, where },
+        fetch
+      ).json();
+
+      filtered = [...r.artworks];
+      total = r.total;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+</script>
+
+<Results />
+
+<div class="stars mx-auto w-full">
+  <Gallery bind:filtered bind:total bind:loadMore />
 </div>
 
-<style lang="scss">
-  @import "../styleguide/theme.scss";
+<style>
+  .stars {
+    background: url("/stars.png");
+    margin-top: 10rem;
+  }
 
-  .secondary-font {
-    font-family: "Audiowide", sans-serif;
+  @media only screen and (max-width: 767px) {
+    .primary-btn {
+      width: 300px;
+      text-align: center;
+      margin: 0 auto;
+      margin-bottom: 30px;
+    }
   }
 </style>
