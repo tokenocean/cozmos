df710d0d486cbd0bf91ea6e54916afc72ef92d57
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-01-28 21:07:40 -0700

    Fix avatar in comments

--- src/routes/a/_comments.svelte
+++ src/routes/a/_comments.svelte
@@ -16,23 +16,23 @@
     comment = "";
     loading = false;
   }
-
 </script>
 
 <div class="text-2xl font-bold mt-12">Comments</div>
 <div class="border rounded-lg p-10 mt-4">
-	{#if !artwork.comments.length}
-	<p>Be the first to leave a comment!</p>
-	{/if}
+  {#if !artwork.comments.length}
+    <p>Be the first to leave a comment!</p>
+  {/if}
   {#each artwork.comments as comment}
     <div class="flex mb-4">
-      <a href="https://www.cozmos.com/{comment.user.username}"><Avatar
-          user={comment.user}
-          size="large" /></a>
+      <a href="https://www.cozmos.com/{comment.user.username}"
+        ><Avatar user={comment.user} size="lg" /></a
+      >
       <div class="ml-10">
         <div class="font-bold">
-          <a
-            href="https://www.cozmos.com/{comment.user.username}">@{comment.user.username}</a>
+          <a href="https://www.cozmos.com/{comment.user.username}"
+            >@{comment.user.username}</a
+          >
         </div>
         <div>{comment.comment}</div>
         <div class="text-sm text-gray-400">
