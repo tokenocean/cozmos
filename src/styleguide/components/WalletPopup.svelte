79a8519d57baa0b22189e530dcee830f1c958c31
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-02-01 16:22:12 -0700

    Get click to copy wallet address working

--- src/styleguide/components/WalletPopup.svelte
+++ src/styleguide/components/WalletPopup.svelte
@@ -1,90 +1,107 @@
 <script>
-    import Fa from 'svelte-fa';
-    import {faUser, faCopy} from '@fortawesome/free-solid-svg-icons';
-    import { hasura } from "$lib/api";
-    import { getArtworksByOwner } from "$queries/artworks";
-    import { asset, balances, user, token } from "$lib/store";
-    import { assetLabel, btc, val, copy } from "$lib/utils";
-    import { getBalances } from "$lib/wallet";
-    import { requireLogin } from "$lib/auth";
-    import { page } from "$app/stores";
+  import Fa from "svelte-fa";
+  import { faUser, faCopy } from "@fortawesome/free-solid-svg-icons";
+  import { hasura } from "$lib/api";
+  import { getArtworksByOwner } from "$queries/artworks";
+  import { asset, balances, user, token } from "$lib/store";
+  import { assetLabel, btc, val, copy } from "$lib/utils";
+  import { getBalances } from "$lib/wallet";
+  import { requireLogin } from "$lib/auth";
+  import { page } from "$app/stores";
 
-    if (!$asset) $asset = btc;
+  if (!$asset) $asset = btc;
 
-    let balance, loading;
-    balances.subscribe((b) => b && (balance = val($asset, b[$asset] || 0)));
+  let balance, loading;
+  balances.subscribe((b) => b && (balance = val($asset, b[$asset] || 0)));
 
-    let artworks = [];
-    $: init($user);
+  let artworks = [];
+  $: init($user);
 
-    let init = async (u) => {
-      if (!u) return;
-      let { data } = await hasura
+  let init = async (u) => {
+    if (!u) return;
+    let { data } = await hasura
       .auth(`Bearer ${$token}`)
       .post({
         query: getArtworksByOwner($user.id),
       })
       .json();
 
-      if (data) ({ artworks } = data);
-      getBalances();
-      loading = false;
-    };
-
+    if (data) ({ artworks } = data);
+    getBalances();
+    loading = false;
+  };
 </script>
 
-<style lang="scss">
-    @import "../theme";
-    .wallet {
-        font-family: $font-family;
-    }
-</style>
-
-{#if ($user)}
-<div class="wallet z-10 absolute left-1/2 bg-white shadow-md w-80 -ml-40 mt-16 rounded-xl overflow-hidden">
+{#if $user}
+  <div
+    class="wallet z-10 absolute left-1/2 bg-white shadow-md w-80 -ml-40 mt-16 rounded-xl overflow-hidden"
+  >
     <div class="bg-black h-12 p-4 flex items-center justify-between">
-        <div class="flex items-center text-white">
-            <a href="/wallet" class="flex items-center text-white" on:click>
-                <Fa icon={faUser} class="mr-4"/> @{$user.username}
-            </a>
+      <div class="flex items-center text-white">
+        <a href="/wallet" class="flex items-center text-white" on:click>
+          <Fa icon={faUser} class="mr-4" /> @{$user.username}
+        </a>
+      </div>
+      <div
+        class="flex items-center text-white cursor-pointer"
+        on:click={() => copy($user.address)}
+      >
+        <div class="mr-4" on:click>
+          {$user.address.substr(0, 3)}...{$user.address.substr(-3)}
         </div>
-        <div class="flex items-center text-white cursor-pointer" on:click={() => copy($user.address)}>
-            <div class="mr-4" on:click>
-                {$user.address.substr(0,3)}...{$user.address.substr(-3)}
-            </div>
-            <div on:click>
-                <div>
-                    <Fa icon={faCopy} class="cursor-pointer"/>
-                </div>
-            </div>
+        <div
+          on:click={() => {
+            navigator.clipboard.writeText($user.address);
+          }}
+        >
+          <div>
+            <Fa icon={faCopy} class="cursor-pointer" />
+          </div>
         </div>
+      </div>
     </div>
     <div class="p-4 text-black">
-        <div class="border border-gray-300 rounded-lg">
-            <div class="text-center font-semibold text-gray-500 text-xs p-2">
-                Balance
-            </div>
-            <div class="flex font-bold text-lg border-t">
-                <div class="flex-1 text-center p-2 border-r">{balance} {assetLabel($asset)}</div>
-                <div class="flex-1 text-center p-2 ">US$ 0.00</div>
-            </div>
+      <div class="border border-gray-300 rounded-lg">
+        <div class="text-center font-semibold text-gray-500 text-xs p-2">
+          Balance
         </div>
-        <div class="text-black flex items-center mt-2">
-            <div class="flex-1 mr-4">
-                <a href="/wallet/fund">
-                    <button class="w-full text-xs border border-gray-300 text-gray-500 rounded font-semibold py-1" on:click>
-                        Fund
-                    </button>
-                </a>
-            </div>
-            <div class="flex-1 ml-4">
-                <a href="/wallet/withdraw">
-                    <button class="w-full text-xs border border-gray-300 text-gray-500 rounded font-semibold py-1" on:click>
-                        Withdraw
-                    </button>
-                </a>
-            </div>
+        <div class="flex font-bold text-lg border-t">
+          <div class="flex-1 text-center p-2 border-r">
+            {balance}
+            {assetLabel($asset)}
+          </div>
+          <div class="flex-1 text-center p-2 ">US$ 0.00</div>
         </div>
+      </div>
+      <div class="text-black flex items-center mt-2">
+        <div class="flex-1 mr-4">
+          <a href="/wallet/fund">
+            <button
+              class="w-full text-xs border border-gray-300 text-gray-500 rounded font-semibold py-1"
+              on:click
+            >
+              Fund
+            </button>
+          </a>
+        </div>
+        <div class="flex-1 ml-4">
+          <a href="/wallet/withdraw">
+            <button
+              class="w-full text-xs border border-gray-300 text-gray-500 rounded font-semibold py-1"
+              on:click
+            >
+              Withdraw
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 {/if}
+
+<style lang="scss">
+  @import "../theme";
+  .wallet {
+    font-family: $font-family;
+  }
+</style>
