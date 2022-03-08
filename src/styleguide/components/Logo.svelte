2ae858db0474140681407fce9ebeb7bf8bf12516
Adam Soltys <asoltys@gmail.com>
2022-03-08 15:53:59 -0800

    logo png

--- src/styleguide/components/Logo.svelte
+++ src/styleguide/components/Logo.svelte
@@ -1,12 +1,12 @@
 <script>
-    export let graphic = false;
-    export let white = false;
+  export let graphic = false;
+  export let white = false;
 </script>
 
 {#if graphic}
-    <img src="/branding/logo-graphic.png" alt="logo" class="{$$props.class}"/>
+  <img src="/logo-graphic.png" alt="logo" class={$$props.class} />
 {:else if white}
-    <img src="/branding/logo-white.svg" alt="logo" class="{$$props.class}"/>
+  <img src="/branding/logo-white.svg" alt="logo" class={$$props.class} />
 {:else}
-    <img src="/branding/logo.png" alt="logo" class="{$$props.class}"/>
+  <img src="/branding/logo.png" alt="logo" class={$$props.class} />
 {/if}
