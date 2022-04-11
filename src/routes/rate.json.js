d6894b58677796de25c597b4fe10dd1fe90d63cc
Adam Soltys <asoltys@gmail.com>
2022-04-10 22:57:32 -0700

    fallback rate

--- src/routes/rate.json.js
+++ src/routes/rate.json.js
@@ -2,15 +2,14 @@ import wretch from "wretch";
 const coinos = wretch().polyfills({ fetch }).url("https://coinos.io/api");
 export async function get() {
   try {
-    let { USD }= await coinos.url("/rates").get().json();
+    let { USD } = await coinos.url("/rates").get().json();
     return {
       body: { rate: USD },
     };
   } catch (e) {
     console.log(e);
     return {
-      body: {},
-      status: 500,
+      body: { rate: 50000 },
     };
   }
 }
