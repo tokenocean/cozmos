ce9e280d0f2c988d9729909fc38f38a79b1bbd25
Adam Soltys <asoltys@gmail.com>
2022-04-12 23:53:19 -0700

    reduce royalty to 2% on sale

--- src/routes/a/create.svelte
+++ src/routes/a/create.svelte
@@ -100,7 +100,7 @@
       if (!artwork.royalty_recipients.find((e) => e.address === address)) {
         artwork.royalty_recipients.push({
           address,
-          amount: artwork.transferred_at ? 2 : amount,
+          amount,
           name,
           type: royaltyRecipientSystemType,
         });
